ee3a3771ee9b922275ab59afc4ac87a19f172865
Rodrigo <rodmenm@gmail.com>
2023-12-14 12:50:17 +0100

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
 const app = express();
 const port = 3000;
-
-var indexRouter = require("./routes/index")
 
 // Configuración de EJS como motor de plantillas
 app.set('view engine', 'ejs');
@@ -11,8 +11,24 @@ app.set('views', './views');
 // Configura a partir del fichero public
 app.use(express.static('public'));
 
+// Configuración de la sesión de Express
+const memoryStore = new session.MemoryStore();
+app.use(session({
+  secret: 'my-secret',
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore,
+}));
+
+// Configuración del adaptador de Keycloak
+const keycloak = new Keycloak({ store: memoryStore });
+app.use(keycloak.middleware());
+
 // Importamos las rutas
+var indexRouterProtected = require("./routes_protected/index")
+var indexRouter = require("./routes/index")
 app.use("/", indexRouter)
+app.use("/logged", keycloak.protect(), indexRouterProtected);
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,3 +1,7 @@
 exports.login = (req, res, next) => {
   res.render("index");
 };
+
+exports.logged = (req, res, next) => {
+  res.render("logged_index");
+};
