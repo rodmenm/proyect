fc48683fc227f23e7d517f44eccf6799b0336a85
rodmenm <124256703+rodmenm@users.noreply.github.com>
2023-12-15 02:36:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-const express = require('express');
-const session = require('express-session');
-const Keycloak = require('keycloak-connect');
+const express = require("express");
+const session = require("express-session");
+const Keycloak = require("keycloak-connect");
+const keycloakConfig = require("./keycloak.json");
 const app = express();
 const port = 3000;
 
 // Configuración de EJS como motor de plantillas
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // Configura a partir del fichero public
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Configuración de la sesión de Express
 const memoryStore = new session.MemoryStore();
-app.use(session({
-  secret: 'my-secret',
-  resave: false,
-  saveUninitialized: true,
-  store: memoryStore,
-}));
+app.use(
+  session({
+    secret: "asdawda3dbqlhbdwoabyo8dygy2o8dadiuoas",
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore,
+  })
+);
 
 // Configuración del adaptador de Keycloak
 const keycloak = new Keycloak({ store: memoryStore });
 app.use(keycloak.middleware());
 
 // Importamos las rutas
-var indexRouterProtected = require("./routes_protected/index")
-var indexRouter = require("./routes/index")
-app.use("/", indexRouter)
+var indexRouterProtected = require("./routes_protected/index");
+var indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 app.use("/logged", keycloak.protect(), indexRouterProtected);
 
 app.listen(port, () => {
