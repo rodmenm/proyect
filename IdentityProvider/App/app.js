49de05665461f03e2cd2efc0afc2ffe092b431c7
Rodrigo Menéndez Muñoz <rodmenm@gmail.com>
2024-05-16 15:56:57 +0200

    Minor_fix

--- IdentityProvider/App/app.js
+++ IdentityProvider/App/app.js
@@ -5,41 +5,16 @@ import { fileURLToPath } from "url";
 import routes from "./routes/routes.js";
 import bodyParser from "body-parser";
 
-import { HolderFinal } from "./../Holder/Holder.js";
-import { IssuerFinal } from "./../Issuer/Issuer.js";
-import { VerifierFinal } from "./../Verifier/Verifier.js";
-
 // Obtener la ruta del directorio actual
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 4000;
 
 //FUNCIONES
-async function issuer_did() { //Crea un DID para el ISSUER
-  try {
-    await Issuer.issuerFinal.agent.dids.create({
-      method: "cheqd",
-      secret: {
-        verificationMethod: {
-          id: "key-1",
-          type: "Ed25519VerificationKey2020",
-        },
-      },
-      options: {
-        network: "testnet",
-        methodSpecificIdAlgo: "uuid",
-      },
-    });
-    console.log("Issuer DID creado correctamente");
-  } catch (error) {
-    console.error("Error al crear el Issuer DID:", error);
-  }
-}
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-global.w_id = "WalletCrear";
-global.w_key = "walletkeycrear0000";
 
 // Configuración de EJS como motor de plantillas
 app.set("view engine", "ejs");
